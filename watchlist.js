b13c69434ebd493872b4d8abc305b9a9ac64a394
Dev Twenty Seven <djpitbullgja@gmail.com>
2025-09-20 05:03:13 -0300

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -1,12 +1,9 @@
-// 
-const watchlist = [
-  "So11111111111111111111111111111111111111112", // SOL
-  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", // USDC
-  "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB", // USDT
+// watchlist.js
+const tokensWatchlist = [
   "9A2jUbgoDY97fruKHXvDd7eQiq4xvnW3By1BfH1Bwn9Y", // DEEP
-  "J3oWJafxg8xmH5vvwBAVX99rmr8b7C9BxjmjaMgCpump", // MAHA
+  "HTU9PssaFxVBXM8KphyVqLeixrUVuubffAHBp9vRpump", // MAHA
   "AtPNZ3Rwcezea16xkjkxULzCMKpSbS2rVEirVfzrnEa6", // GOLD
   "vtUzXyowSMsNUwiZMLNufySoeW9ehaPAVr7YsYQpump", // MARV
   "FRsV3m924aGpLMuEekoo3JkkMt1oopaM4JY9ki5YLXrp"  // XVM
-  // 
+  //
 ];
