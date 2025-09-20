c6f31bd10847bd88b70c243957c5eb9513006948
Dev Twenty Seven <djpitbullgja@gmail.com>
2025-09-20 05:59:24 -0300

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -1,9 +1,16 @@
 // watchlist.js
 const tokensWatchlist = [
+  //"So11111111111111111111111111111111111111112", // SOL/USD1
+  "Dz9mQ9NzkBcCsuGPFJ3r1bS4wgqKMHBPiVuniW8Mbonk", // USELESS
+  "6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN", // TRUMP
   "9A2jUbgoDY97fruKHXvDd7eQiq4xvnW3By1BfH1Bwn9Y", // DEEP
-  "HTU9PssaFxVBXM8KphyVqLeixrUVuubffAHBp9vRpump", // MAHA
+  "5UUH9RTDiSpq6HKS6bp4NdU9PNJpXRXuiw6ShBTBhgH2", // TROLL
+  "9AvytnUKsLxPxFHFqS6VLxaxt5p6BhYNr53SD2Chpump", // 67
   "AtPNZ3Rwcezea16xkjkxULzCMKpSbS2rVEirVfzrnEa6", // GOLD
+  "DKffyw1oEVWR3ik7q7tb2shhmZ3jbTkRzT6R5banCxai", // AI20X
+  "7Pnqg1S6MYrL6AP1ZXcToTHfdBbTB77ze6Y33qBBpump", // BagWork
   "vtUzXyowSMsNUwiZMLNufySoeW9ehaPAVr7YsYQpump", // MARV
-  "FRsV3m924aGpLMuEekoo3JkkMt1oopaM4JY9ki5YLXrp"  // XVM
+  "FRsV3m924aGpLMuEekoo3JkkMt1oopaM4JY9ki5YLXrp",  // XVM
+  "Bb43TPQQh5TaoCtFLGjfwAhqcLumN6mTUmCy2ojjpump" // RETRO
   //
 ];
