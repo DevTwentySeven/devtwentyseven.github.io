f7f7b70d1a89cef6a55870a8fc708f2945365a6d
Dev Twenty Seven <djpitbullgja@gmail.com>
2025-10-01 20:33:16 -0300

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -3,7 +3,6 @@ const tokensWatchlist = [
   //"So11111111111111111111111111111111111111112", // SOL/USD1
   "Dz9mQ9NzkBcCsuGPFJ3r1bS4wgqKMHBPiVuniW8Mbonk", // USELESS
   "6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN", // TRUMP
-  "GEm7mheJTkY4Sa6ZaSiQjsQ8aprjrtHUZVj478vkpump", // PEPITO
   "9A2jUbgoDY97fruKHXvDd7eQiq4xvnW3By1BfH1Bwn9Y", // DEEP
   "5UUH9RTDiSpq6HKS6bp4NdU9PNJpXRXuiw6ShBTBhgH2", // TROLL
   "9AvytnUKsLxPxFHFqS6VLxaxt5p6BhYNr53SD2Chpump", // 67
@@ -12,6 +11,7 @@ const tokensWatchlist = [
   "7Pnqg1S6MYrL6AP1ZXcToTHfdBbTB77ze6Y33qBBpump", // BagWork
   "vtUzXyowSMsNUwiZMLNufySoeW9ehaPAVr7YsYQpump", // MARV
   "FRsV3m924aGpLMuEekoo3JkkMt1oopaM4JY9ki5YLXrp",  // XVM
+  "DGbRsRZuXZgwXPxy6ZPwunQYZwa7b3gfC9AnBcgepump", // MAHA
   "Bb43TPQQh5TaoCtFLGjfwAhqcLumN6mTUmCy2ojjpump" // RETRO
   //
 ];
